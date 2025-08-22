c9110d3c6719b2cf255676abfe7aad5b3dc0e6e6
seth-skocelas <s.skocelas@gmail.com>
2025-08-22 14:04:14 -0500

    Initial Commit

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Seth Skocelas on 8/22/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Netflix CloneTests/Netflix_CloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  Netflix_CloneTests.swift
+//  Netflix CloneTests
+//
+//  Created by Seth Skocelas on 8/22/25.
+//
+
+import Testing
+@testable import Netflix_Clone
+
+struct Netflix_CloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
